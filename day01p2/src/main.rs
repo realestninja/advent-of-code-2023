5bd3eaabeba69ba407a401a0f973b234fc6f7106
Nikhil Nawgiri <the@realest.ninja>
2023-12-03 21:53:49 +0100

    WIP

--- day01p2/src/main.rs
+++ day01p2/src/main.rs
@@ -30,23 +30,26 @@ fn main() {
     let number_map = create_number_map();
 
     for line in contents.lines() {
-        let mut indices_of_first_and_last_numbers_found: [i8; 2] = [-1, -1];
+        let mut indices_of_first_and_last_numeric_numbers: [i8; 2] = [-1, -1];
+        let mut indices_of_first_and_last_string_numbers: [i8; 2] = [-1, -1];
+
         let mut values_of_first_and_last_numbers_as_strings: [String; 2] = Default::default();
 
         let mut char_index = 0;
         for c in line.chars() {
             if c.is_digit(10) {
-                let first_number_otherwise_last = if indices_of_first_and_last_numbers_found[0] < 0 { 0 } else { 1 };
-                indices_of_first_and_last_numbers_found[first_number_otherwise_last] = char_index;
+                let first_number_otherwise_last = if indices_of_first_and_last_numeric_numbers[0] < 0 { 0 } else { 1 };
+                indices_of_first_and_last_numeric_numbers[first_number_otherwise_last] = char_index;
             }
             // to do: p2 task, find numbers written as string
+            // search string for each substring
 
             char_index += 1;
         }
 
         // handle first value
-        if indices_of_first_and_last_numbers_found[0] >= 0 {
-            let first_value = line.chars().nth(indices_of_first_and_last_numbers_found[0] as usize);
+        if indices_of_first_and_last_numeric_numbers[0] >= 0 {
+            let first_value = line.chars().nth(indices_of_first_and_last_numeric_numbers[0] as usize);
             let value = match first_value {
                 Some(v) => v,
                 None => panic!("No value found"),
@@ -55,8 +58,8 @@ fn main() {
         }
 
         // handle second value
-        if indices_of_first_and_last_numbers_found[1] >= 0 {
-            let second_value = line.chars().nth(indices_of_first_and_last_numbers_found[1] as usize);
+        if indices_of_first_and_last_numeric_numbers[1] >= 0 {
+            let second_value = line.chars().nth(indices_of_first_and_last_numeric_numbers[1] as usize);
             let value = match second_value {
                 Some(v) => v,
                 None => panic!("No value found"),
