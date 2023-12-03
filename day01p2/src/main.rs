05a3f7ec1a67c0399951e5ee25a3fd970e97a371
Nikhil Nawgiri <the@realest.ninja>
2023-12-03 21:04:39 +0100

    Rewrite logic - WIP

--- day01p2/src/main.rs
+++ day01p2/src/main.rs
@@ -28,33 +28,42 @@ fn main() {
         let mut array_of_indices: [i8; 2] = [-1, -1];
         let mut array_of_first_and_last_digit: [String; 2] = Default::default();
 
-        // remake from here
-        // get indices of first and last number
-        // get indices of beginning of substring
-
         let mut char_index = 0;
         for c in line.chars() {
             if c.is_digit(10) {
                 let array_of_indices_current_index = if array_of_indices[0] < 0 { 0 } else { 1 };
                 array_of_indices[array_of_indices_current_index] = char_index;
-
-                // old
-                let digit_index = if array_of_first_and_last_digit[0].is_empty() { 0 } else { 1 };
-                array_of_first_and_last_digit[digit_index] = c.to_string();
             }
             char_index += 1;
         }
 
+        // handle first value that was found
+        if array_of_indices[0] >= 0 {
+            let first_value = line.chars().nth(array_of_indices[0] as usize);
+            let value = match first_value {
+                Some(v) => v,
+                None => panic!("No value found"),
+            };
+            println!("value: {:?}", value);
+            array_of_first_and_last_digit[0] = value.to_string();
+        }
+
+        // handle second value if it was found
+        // to do
+
         // handle case where there is only one number
-        if array_of_first_and_last_digit[1].is_empty() {
+        if array_of_indices[1] < 0 {
             array_of_first_and_last_digit[1] = array_of_first_and_last_digit[0].clone()
         }
 
+        println!("array_of_first_and_last_digit: {:?}", array_of_first_and_last_digit);
+
         // remake end
         calibration_value_digits.push(array_of_first_and_last_digit.join("").parse::<i32>().unwrap());
-        println!("array_of_indices: {:?}", array_of_indices);
+        // println!("array_of_indices: {:?}", array_of_indices);
+        // println!("array_of_first_and_last_digit: {:?}", array_of_first_and_last_digit);
     }
 
-    let sum: i32 = calibration_value_digits.iter().sum();
-    println!("sum: {:?}", sum);
+    // let sum: i32 = calibration_value_digits.iter().sum();
+    // println!("sum: {:?}", sum);
 }
