51dc2d471ddd5c0911bd62c4f77f3aa33cd1232a
Nikhil Nawgiri <the@realest.ninja>
2023-12-11 08:17:03 +0100

    Refactor and remove console outputs used for debugging

--- day01p2/src/main.rs
+++ day01p2/src/main.rs
@@ -3,7 +3,7 @@ use std::io::prelude::*;
 use std::collections::HashMap;
 
 fn create_number_map() -> HashMap<&'static str, u8> {
-    let mut number_map: HashMap<&'static str, u8> = [
+    let number_map: HashMap<&'static str, u8> = [
         ("one", 1),
         ("two", 2),
         ("three", 3),
@@ -28,8 +28,6 @@ fn main() {
     let mut calibration_value_digits: Vec<i32> = Vec::new();
 
     let number_map = create_number_map();
-    println!("number_map: {:?}", number_map);
-    println!("number_map: {:?}", number_map.keys());
 
     for line in contents.lines() {
         let mut collection_of_all_values_and_their_index: Vec<[i8; 2]> = Vec::new();
@@ -44,7 +42,6 @@ fn main() {
         }
 
         // search for substring numbers
-        println!("line: {:?}", line);
         for key in number_map.keys() {
             if line.contains(key) {
                 for (index, _) in line.match_indices(key) {
@@ -57,10 +54,6 @@ fn main() {
                 }
             }
         }
-
-        // add 2-digit-string to vector where all 2-digit-strings are collected
-        println!("collection_of_all_values_and_their_index: {:?}", collection_of_all_values_and_their_index);
-        println!("collection_of_all_values_and_their_index: {:?}", collection_of_all_values_and_their_index.iter());
 
         // Iterate through the vector and find the minimum value in the first dimension (index 0)
         let mut min_value = i8::MAX; // Initialize with the maximum possible value
@@ -84,19 +77,11 @@ fn main() {
             }
         }
 
-        let min_element = collection_of_all_values_and_their_index[min_index];
-        let max_element = collection_of_all_values_and_their_index[max_index];
-
-        println!("Element with the minimum value: {:?}", min_element[1]);
-        println!("Element with the maximum value: {:?}", max_element[1]);
-
         let mut values_of_first_and_last_numbers_as_strings: [String; 2] = Default::default();
         values_of_first_and_last_numbers_as_strings[0] = collection_of_all_values_and_their_index[min_index][1].to_string();
         values_of_first_and_last_numbers_as_strings[1] = collection_of_all_values_and_their_index[max_index][1].to_string();
 
-        println!("values_of_first_and_last_numbers_as_strings: {:?}", values_of_first_and_last_numbers_as_strings);
         calibration_value_digits.push(values_of_first_and_last_numbers_as_strings.join("").parse::<i32>().unwrap());
-        println!("------------------------------------");
     }
 
     let sum: i32 = calibration_value_digits.iter().sum();
