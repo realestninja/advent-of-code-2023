7d3cb21d8f8d6df5b2f822ce2f7b6413cf398b3f
Nikhil Nawgiri <the@realest.ninja>
2023-12-13 12:06:23 +0100

    Create day03p1 folder with example_input and puzzle_input

---
+++ day03p1/src/main.rs
@@ -0,0 +1,13 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let mut file = File::open("example_input").expect("File not found");
+    // let mut file = File::open("puzzle_input").expect("File not found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Error reading file");
+
+    for line in contents.lines() {
+        println!("line: {:?}", line);
+    }
+}
